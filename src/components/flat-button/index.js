2d74052df5090f8d1dfd4d67aa7c9b8aca2ad022
Rafael Dellaquila <rafadellaquila@gmail.com>
2019-10-04 00:13:16 -0300

    refatorando btn pras cores dinamicas direto no css

--- src/components/flat-button/index.js
+++ src/components/flat-button/index.js
@@ -1,14 +1,16 @@
 const flatButton = (() => {
   const module = {};
 
-  module._style = () => {
+  module._id = 0;
+
+  module._style = active => {
     const $head = document.querySelector("head");
     const $style = document.createElement("style");
 
     $style.textContent = `
-        .flat-button {
-            background-color: #eae6da;
-            color: #fffcee;
+        .flat-button-${module._id} {
+            background-color: ${active ? "#f25a70" : "#eae6da"};
+            color: ${active ? "#fff" : "#fffcee"};
             width: 50%;
             height: 176px;
             text-transform: uppercase;
@@ -16,15 +18,14 @@ const flatButton = (() => {
             font-weigth: bold;
             border: none;
         }
-        .flat-button.-active {
-          background-color: #f25a70;
-        }
+        
     `;
     $head.insertAdjacentElement("beforeend", $style);
   };
-  module.render = (content, variation) => {
-    module._style();
-    return `<button class="flat-button ${variation}">${content}</button>`;
+  module.render = (content, active = false) => {
+    module._id++;
+    module._style(active);
+    return `<button class="flat-button-${module._id}">${content}</button>`;
   };
   return {
     render: module.render
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,7 +1,7 @@
 (() => {
   const $root = document.querySelector("#root");
   $loginButton = flatButton.render("log in");
-  $signupButton = flatButton.render("sign up", "-active");
+  $signupButton = flatButton.render("sign up", true);
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signupButton);
